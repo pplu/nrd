dbd9e2a2e791a7cd55ceea410d3dd9ad4782a834
pplusdomain <pplusdomain@53839ed6-144d-1558-62e9-4f4b8b0e5f8d>
2010-01-26 21:48:34 +0000

    Refactor code to make future serializers pluggable without modifying code
    t/*.cfg
     The configs change their layout
     encrypt key states which serailizer subclass to use
     new encrypt_type sets the encription type for the crypt serializer
     encrypt_key still holds the key
    
    Serialize.pm
     - returns instances of NRD::Serializer::XXX classes
    
    Serialize/*.pm
     - serializer classes for encrypted and unencrypted communication
    
    Spec for classes in the NRD::Serializer namespace
     new. Will be called with the configuration hash
     needs_helo. Returns 0 or 1 if the serializer needs a leading packet sent to it.
     helo. The NRD daemon will pass the leading packet contents to this method
           when a client connects
     freeze. Will serialize the contents of it's first argument, and return the resulting
             serialization
     unfreeze. Will unserialize it's first argument, and return it.
    
    NRD/Daemon.pm
    bin/send_nrd
     Refactor the instancing of serializers, and the treatment of 
     HELO packets
    
     Handle the new config keys in the daemon, too...
    
    MANIFEST
     - declare the new files

--- lib/NRD/Serialize/crypt.pm
+++ lib/NRD/Serialize/crypt.pm
@@ -1,38 +1,45 @@
-package NRD::SerializeCrypt;
+package NRD::Serialize::crypt;
 
-use base 'NRD::Serialize';
+use Carp;
+
+# overrides freeze and unfreeze methods, encrypting and
+# unencrypting data first
+use base 'NRD::Serialize::none';
 
 sub new {
   my ($class, $options) = @_;
   $options = {} if (not defined $options);
   my $self = {
-    'encrypt' => undef,
+    'encrypt_type' => undef,
     'encrypt_key' => undef,
     %$options
   };
 
   bless($self, $class);
 
-  die 'No encryption specified' if (not defined $self->{'encrypt'});
+  die 'No encrypt_type specified' if (not defined $self->{'encrypt_type'});
   die 'No encrypt_key specified' if (not defined $self->{'encrypt_key'});
 
   require Crypt::CBC or die "Can't load Crypt::CBC";
   $self->{'iv'} = Crypt::CBC->random_bytes(8)
     if (not defined $self->{'iv'});
-  my $td = Crypt::CBC->new( -cipher => $self->{'encrypt'},
+  my $td = Crypt::CBC->new( -cipher => $self->{'encrypt_type'},
                             -key => $self->{'encrypt_key'},
                             -iv  => $self->{'iv'},
                             -header => 'none'
-           ) or die "Can't load cipher '$self->{'encrypt'}'";
+           ) or die "Can't load cipher '$self->{'encrypt_type'}'";
 
   $self->{'td'} = $td;
   return $self;
 }
 
-sub iv {
-  my ($self, $iv) = @_;
-  return ($self->{'td'}->iv) if (not defined $iv);
-  $self->{'td'}->iv($iv);
+sub needs_helo { 1 }
+
+sub helo {
+  my ($self, $helo) = @_;
+  # The IV is sent in the helo packet
+  $self->{'td'}->iv($helo) if (defined $helo);
+  return $self->{'td'}->iv;
 }
 
 sub freeze {
@@ -44,11 +51,8 @@ sub freeze {
 
 sub unfreeze {
   my ($self, $string) = @_;
-  #print Dumper($string) . " length:" . length($string);
   my $dec = $self->{'td'}->decrypt($string);
-  #$dec =~ s/\x00*^//g;
-  #print Dumper($dec) . " length:" . length($dec);
-  return ($self->SUPER::unfreeze($dec));
+  $self->SUPER::unfreeze($dec);
 }
 
 1;
