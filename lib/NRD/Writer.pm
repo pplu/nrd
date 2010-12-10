de4143ff02366039fc35c2151901ed3dc6d67fbf
pplusdomain <pplusdomain@53839ed6-144d-1558-62e9-4f4b8b0e5f8d>
2010-12-10 16:27:56 +0000

    Move code that writes results to a separate class, making it pluggable

---
+++ lib/NRD/Writer.pm
@@ -0,0 +1,24 @@
+package NRD::Writer;
+
+# Base class for components that write results to Nagios
+#
+
+use strict;
+use warnings;
+
+sub instance_of {
+  my (undef, $type, @args) = @_;
+  my $class = 'NRD::Writer::' . $type;
+  {
+   my $file = $class;
+   $file =~ s/\:\:/\//g;
+   require "$file.pm";
+  }
+  use Module::Load;
+  load $class;
+  #$class::import;
+
+  return $class->new(@args);
+}
+
+1;
